7dcbd1e31872581b74203982012cfcea91aa8291
Deven Blackburn <hkdeven@users.noreply.github.com>
2016-04-20 08:55:08 -0400

    Updated sequence of methods

--- vs_menumethods.rb
+++ vs_menumethods.rb
@@ -1,7 +1,105 @@
 #These are all methods used in voter_simulation.rb
+
+#This method compares the users response to the list of valid responses and outputs an error and main menu return if not valid. LOVE THIS!!!
+  def verified_answer(user_response, list_of_responses)
+    if list_of_responses.include? user_response
+       user_response
+    else
+       invalid_return_mm
+    end
+  end
+
+# This method stores whole words for politicians political affiliation.
+  def whole_affil (new_party_affiliation)
+    if new_party_affiliation == "d"
+      "Democrat"
+    else new_party_affiliation == "r"
+      "Republican"
+    end
+  end
+
+#This method stores whole words for voters political view.
+  def whole_views(new_political_view)
+    case new_political_view
+    when "l"
+      "Liberal"
+    when "c"
+      "Conservative"
+    when "t"
+      "Tea Party"
+    when "s"
+      "Socialist"
+    when "n"
+      "Neutral"
+    else
+      "Liberal"
+    end
+  end
+
+  def line_break_50
+    american_line = ((("=".blue)+("=".red))*40) + ("=".blue)
+    puts american_line
+  end
+
+  def security_check
+    puts
+    slow_r_down "RETINAL AND BIOMETRIC SCAN CONFIRMED.".center(80)
+    slow_r_down "WELCOME MADAME INSPECTOR GENERAL LYNN McFARLAND.".center(80).blue
+    puts
+    puts
+  end
+
+# This slows down the text, all spy like.  ;)
+  def slow_r_down(text)
+    text.to_s.each_char do |x|
+      print x
+      case x
+      when ".", "!", "?"
+        sleep(0.35)
+      else
+        sleep(0.03)
+      end
+    end
+  end
+
+  #If user selects an invalid option it takes them back to main menu.
+  def invalid_return_mm
+    puts "Invalid option : No match found.  Returning to the main menu.".center(80)
+    main_menu
+  end
+
+  def menu_header
+    line_break_50
+    puts "FEDERAL ELECTION COMMISSION: MAIN MENU".center(80)
+    line_break_50
+  end
+
+  def main_menu
+    puts """
+    Enter C to create a new voter/politician,
+    L to list current voter/politician,
+    U to update an existing voter/politician,
+    or D to delete.
+    """.center(80)
+    menu_selection = verified_answer(gets.chomp.downcase, ["c", "l", "u", "d"])
+    case menu_selection
+    when "c"
+      create_option
+    when "l"
+      list_option
+    when "u"
+      update_option
+    else "d"
+      delete_option
+    end
+    menu_header
+    main_menu
+  end
+
+#The following methods update a voter or politician in the directory.
 def update_option
    puts "Would you like to Update a (V)oter or (P)olitician record?".center(80)
-   update_type = goliath(gets.chomp.downcase, ["p", "v"])
+   update_type = verified_answer(gets.chomp.downcase, ["p", "v"])
      if update_type == "p"
        update_politician
        list_politicians
@@ -52,7 +150,7 @@ def initiate_update_p(enum)
   @politician_directory[enum].name = gets.chomp.downcase
   puts "Enter correct party affiliation:".center(80)
   puts "(R)epublican or (D)emocrat?".center(80)
-  new_party_affiliation = goliath(gets.chomp.downcase, ["d", "r"])
+  new_party_affiliation = verified_answer(gets.chomp.downcase, ["d", "r"])
   updated_full_affil = whole_affil (new_party_affiliation)
   @politician_directory[enum].party_affiliation = updated_full_affil.downcase
 end
@@ -65,114 +163,21 @@ def initiate_update_v(enum)
   POLITICAL VIEW:
   (L)iberal, (C)onservative, (T)ea Party,
   (S)ocialist, or (N)eutral.""".center(80)
-  new_political_v = goliath(gets.chomp.downcase, ["l", "c", "t", "s", "n"])
+  new_political_v = verified_answer(gets.chomp.downcase, ["l", "c", "t", "s", "n"])
   updated_political_v = whole_views (new_political_v)
   @voter_directory[enum].political_view = updated_political_v.downcase
 end
 
-#############################################################
-
-  def goliath(user_response, list_of_responses)
-    if list_of_responses.include? user_response
-       user_response
-    else
-       invalid_return_mm
-    end
-  end
-
-  def whole_affil (new_party_affiliation)
-    if new_party_affiliation == "d"
-      "Democrat"
-    else new_party_affiliation == "r"
-      "Republican"
-    end
-  end
-
-  def whole_views(new_political_view)
-    case new_political_view
-    when "l"
-      "Liberal"
-    when "c"
-      "Conservative"
-    when "t"
-      "Tea Party"
-    when "s"
-      "Socialist"
-    when "n"
-      "Neutral"
-    else
-      "Liberal"
-    end
-  end
-
-  def line_break_50
-    american_line = ((("=".blue)+("=".red))*40) + ("=".blue)
-    puts american_line
-  end
-
-  def security_check
-    puts
-    slow_r_down "RETINAL AND BIOMETRIC SCAN CONFIRMED.".center(80)
-    slow_r_down "WELCOME MADAME INSPECTOR GENERAL LYNN McFARLAND.".center(80).blue
-    puts
-    puts
-  end
-
-  def slow_r_down(text)
-    text.to_s.each_char do |x|
-      print x
-      case x
-      when ".", "!", "?"
-        sleep(0.35)
-      else
-        sleep(0.03)
-      end
-    end
-  end
-
-  #If user selects an invalid option it takes them back to main menu.
-  def invalid_return_mm
-    puts "Invalid option : No match found.  Returning to the main menu.".center(80)
-    main_menu
-  end
-
-  def menu_header
-    line_break_50
-    puts "FEDERAL ELECTION COMMISSION: MAIN MENU".center(80)
-    line_break_50
-  end
-
-  def main_menu
-    puts """
-    Enter C to create a new voter/politician,
-    L to list current voter/politician,
-    U to update an existing voter/politician,
-    or D to delete.
-    """.center(80)
-    menu_selection = goliath(gets.chomp.downcase, ["c", "l", "u", "d"])
-    case menu_selection
-    when "c"
-      create_option
-    when "l"
-      list_option
-    when "u"
-      update_option
-    else "d"
-      delete_option
-    end
-    menu_header
-    main_menu
-  end
-
+# These methods create a new voter or politician.
   def create_option
     puts "Create new (P)olitician or (V)oter?".center(80)
-    creation_type = goliath(gets.chomp.downcase, ["p", "v"])
+    creation_type = verified_answer(gets.chomp.downcase, ["p", "v"])
     if creation_type.downcase == "p"
       puts "Enter new politician details.".center(80)
       puts "NAME:".center(80)
       new_name = gets.chomp.downcase
       puts "PARTY AFFILIATION: (R)epublican or (D)emocrat?".center(80)
-      new_party_affiliation = goliath(gets.chomp.downcase, ["d", "r"])
+      new_party_affiliation = verified_answer(gets.chomp.downcase, ["d", "r"])
       @politician_directory << Politician.new(new_name, whole_affil(new_party_affiliation))
       list_politicians
     else
@@ -183,7 +188,7 @@ end
       POLITICAL VIEW:
       (L)iberal, (C)onservative, (T)ea Party,
       (S)ocialist, or (N)eutral.""".center(80)
-      new_political_view = goliath(gets.chomp.downcase, ["l", "c", "t", "s", "n"])
+      new_political_view = verified_answer(gets.chomp.downcase, ["l", "c", "t", "s", "n"])
       @voter_directory << Voter.new(new_name, whole_views(new_political_view))
       list_voters
     end
@@ -191,6 +196,7 @@ end
     puts
   end
 
+# These methods list records in the directory.
   def list_option
     list_politicians
     list_voters
@@ -217,10 +223,11 @@ end
     end
   end
 
+# These methods delete records from the directory.
   def delete_option
     puts "PROCEED WITH CAUTION".center(80).red
     puts "Delete (P)olitician or (V)oter record?".center(80)
-    delete_type = goliath(gets.chomp.downcase, ["p", "v"])
+    delete_type = verified_answer(gets.chomp.downcase, ["p", "v"])
        if delete_type == "p"
          delete_politician
          list_politicians
@@ -270,93 +277,3 @@ end
         invalid_return_mm
     end
   end
-  # def delete_politician
-  #       list_politicians
-  #       puts
-  #       puts "To delete POLITICIAN record, enter NAME as it appears above:".center(80)
-  #       deletion_named = gets.chomp.downcase
-  #       puts
-  #       puts "Are you sure you want to delete the politician record for #{clean_r_up(deletion_named)}?".center(80).red
-  #       puts "Enter (Y)es or (N)o.".center(80).red
-  #       confirm_deletion_named = gets.chomp.downcase
-  #         if confirm_deletion_named.include? "y"
-  #           @politician_directory.delete_if {|x| clean_r_up(x.name) == clean_r_up(deletion_named)}
-  #           puts "Politician record for #{clean_r_up(deletion_named)} DELETED.".center(80)
-  #         else
-  #           puts "Deletion action aborted.  Returning to main menu.".center(80)
-  #         else
-  #           invalid_return_mm
-  #         end
-
-          # def initiate_update_p(enum)
-          #   puts "Enter correct name:".center(80)
-          #   @politician_directory[enum].name = gets.chomp
-          #   puts "Enter correct party affiliation:".center(80)
-          #   puts "(R)epublican or (D)emocrat?".center(80)
-          #   new_party_affiliation = goliath(gets.chomp.downcase, ["d", "r"])
-          #   updated_full_affil = whole_affil (new_party_affiliation)
-          #   @politician_directory[enum].party_affiliation = updated_full_affil.downcase
-          # end
-  #
-  #     else
-  #       list_voters
-  #       puts
-  #       puts "To delete VOTER record, enter NAME exactly as it appears in directory.".center(80)
-  #       deletion_named = gets.chomp.downcase
-  #       puts "Are you sure you want to delete the voter record for #{clean_r_up(deletion_named)}?".center(80).red
-  #       puts "Enter (Y)es or (N)o.".center(80).red
-  #       confirm_deletion_named = gets.chomp.downcase
-  #         if confirm_deletion_named.include? "y"
-  #           @voter_directory.delete_if {|y| clean_r_up(y.name) == clean_r_up(deletion_named.downcase)}
-  #           puts "Voter record for #{clean_r_up(deletion_named)} DELETED.".center(80)
-  #         elsif confirm_deletion_named.include? "n"
-  #           puts "Deletion action aborted.  Returning to main menu.".center(80)
-  #         else
-  #           invalid_return_mm
-  #         end
-  #     end
-  # end
-
-=begin
-
-Unsuccessful method attempts:
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-#Attempt to include an image in the command line:
-#!/usr/bin/env ruby
-
-require "tco"
-require "rmagick"
-
-Magick::Image.read("tux.png")[0].each_pixel do |pixel, col, row|
-  c = [pixel.red, pixel.green, pixel.blue].map { |v| 255*(v/65535.0) }
-  print "  ".bg c
-  puts if col >= 53
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# This was in my world class, but didn't do anything:
-# def self.voter_directory
-#   @voter_directory
-# end
-# def self.politician_directory
-#   @politician_directory
-# end
-
-#Why doesn't this work???
-def update_vname
-  puts "Enter name of the VOTER you would like to update:"
-  match_name2 = gets.chomp.downcase
-  @voter_directory.each do |matchv|
-    if matchv.include? match_name2
-      puts "Enter new name:"
-      new_name1 = gets.chomp.downcase
-      z[0] = new_name1
-    else
-      invalid_return_mm
-    end
-  end
-end
-
-=end
